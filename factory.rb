b8965edabef1b4adea58ea55bec7b7473d59086c
markhaynes456 <markhaynes456@gmail.com>
2015-03-14 19:45:32 -0400

    Added Factory Pattenwqr

---
+++ factory.rb
@@ -0,0 +1,16 @@
+#################
+# Factory Pattern
+#
+# When to use
+# If you do not know what class you will need
+# encapsulate the creation of objects
+# Hide the selection process away from the user 
+# If you need to select one class at random from a list of classes which all contain the same superclass
+#
+# Characteristics
+# Chosen at run time
+# 
+###############
+
+
+
