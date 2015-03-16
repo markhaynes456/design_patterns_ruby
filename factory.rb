ae95197a9dcd94392a13295c8be7c7f2b7505acd
markhaynes456 <markhaynes456@gmail.com>
2015-03-16 17:11:57 -0400

    Complete factory v1

--- factory.rb
+++ factory.rb
@@ -9,8 +9,83 @@
 #
 # Characteristics
 # Chosen at run time
-# 
-###############
+# Random selection at run time
+#
+# Implementation
+#
+#
+##############
+
+module RobotGeneric
+
+	def move
+		raise NotImplementedError
+	end
+
+	def follow(target)
+		raise NotImplementedError
+	end
+
+end
+
+class Robot
+	
+	include RobotGeneric
+
+	attr_reader :name
+
+	@name = "Robot Abstract"
+
+	def move
+		puts("#{@name} moves around")
+	end
+
+	def follow(target)
+		puts("#{@name} follows #{target}")
+	end
+
+	private
+		attr_writer :name
+
+end
 
 
+class KillBot < Robot
 
+	def shoot
+		puts("#{name} shoots in the air")
+	end
+
+end
+
+class HelperBot < Robot
+	
+end
+
+class RobotFactory
+
+	def initialize
+		@robots = []
+		self.register(KillBot)
+		self.register(HelperBot)
+	end
+
+	def register(robot)
+		@robots.push(robot.new)
+	end
+
+	def create_robot(id)
+		@robots[id]
+	end
+
+	def build_robot
+		@robots[rand(@robots.length)]
+	end
+
+	private
+		attr_accessor :robots
+
+end
+
+RobotFactory.new.build_robot.follow("Jone")
+RobotFactory.new.build_robot.follow("Ian")
